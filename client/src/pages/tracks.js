c9bbb317d107f8a77d54ce0a35a24c685e4bf676
Janessa Garrow <janessa@apollographql.com>
2021-10-19 14:38:22 -0600

    Create getTracks query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import {gql} from '@apollo/client';
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
