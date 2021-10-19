e0b4d97a2bf2751bdd0711c1f644110277c11bc8
Janessa Garrow <janessa@apollographql.com>
2021-10-19 14:29:28 -0600

    Create server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-//TODO
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(() => {
+    console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+  });
